8f71a37aa5c06509f7b12d30d1e2dadd376b45aa
BAJARE Atul <U97@dsone.3ds.com>
2024-11-16 17:16:46 +0530

    frst commit

--- src/main/java/com/demo/demo/controller/TestController.java
+++ src/main/java/com/demo/demo/controller/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	@GetMapping("/test")
 	public String getTest() {
-		return "testing codecommit";
+		return "testing codecommit!";
 	}
 }
