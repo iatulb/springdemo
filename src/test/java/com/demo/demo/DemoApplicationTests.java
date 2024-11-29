a3ba2f47c29038d79b7093c7856890ed35f50520
BAJARE Atul <U97@dsone.3ds.com>
2024-11-29 17:27:05 +0530

    .

--- src/test/java/com/demo/demo/DemoApplicationTests.java
+++ src/test/java/com/demo/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.demo.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +12,10 @@ class DemoApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+    public void testPrintMessage() {      
+		System.out.println("Inside testPrintMessage()");     
+		assertEquals("Atul", "Atul");
+	}
+	
 }
